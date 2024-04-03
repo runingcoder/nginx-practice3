e68ff7f92e62dbd8e6120c54d60bfb59053ff43d
Achyut Kayastha <achyutkayastha5@gmail.com>
2024-04-03 16:49:47 +0545

    asdfasdf

--- fetch.js
+++ fetch.js
@@ -1,14 +1,29 @@
-fetch('http://128.199.196.16:8004/users/')
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Network response was not ok');
-    }
-  })
-  .then(data => {
-    console.log('Response data:', data);
-  })
-  .catch(error => {
-    console.error('There was an error:', error);
-  });
+// fetch('http://128.199.196.16:8004/users/', {
+//     headers: {
+//         'Origin': '192.168.254.47'
+//     }
+// })
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json();
+//         } else {
+//             throw new Error('Network response was not ok');
+//         }
+//     })
+//     .then(data => {
+//         console.log('Response data:', data);
+//     })
+//     .catch(error => {
+//         console.error('There was an error:', error);
+//     });
+
+
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('origin', 'https://facebook.com');
+fetch('http://128.199.196.16:8004/users/', {
+    headers: headers
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
