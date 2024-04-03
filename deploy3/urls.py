c57f869304859ffb1298693f450c92b99c55b2a6
Achyut Kayastha <achyutkayastha5@gmail.com>
2024-04-03 11:24:05 +0545

    feat: nginx deploy3 to check in server

---
+++ deploy3/urls.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name']
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename= 'userss')
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
